b364e7996178c9ceda99ec8c4b72b2e4f6e1b419
Weaster-TR <sezerpamukcu016@gmail.com>
2024-09-01 13:06:34 +0300

    header.php

---
+++ admin/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Paneli</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">Admin Paneli</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="admin_logout.php">Çıkış Yap</a>
+            </li>
+        </ul>
+    </div>
+</nav>
